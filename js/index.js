bf604cfe2878c19e4e8e7a034082f9552ead11a0
Matheus <matheushcastiglioni@gmail.com>
2016-11-14 11:46:48 -0200

    Acerto no endereço e link para abrir imagens

---
+++ js/index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+    var target = document.getElementsByClassName('js-link')[0];
+    var close = document.getElementsByClassName('js-close')[0];
+
+    let links = document.querySelectorAll('a[data-target][data-type]');
+    if (links != undefined && links.length > 0) {
+        for(let i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', e => {
+                if (target != undefined) {
+                    target.src = `img/pratos/${e.target.dataset.target}.${e.target.dataset.type}`;
+                    target.style.display = 'block';
+                }
+            });
+        }
+    }
+
+    if (close != undefined) {
+        close.addEventListener('click', e => {
+            target.style.display = 'none';
+            target.src = '';
+        })
+    }
+
+});
